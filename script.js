a8c7c28905edcc2100fbb76eb2c8ccf074aebe25
AlienJim <burton.alexander102@gmail.com>
2017-02-02 22:36:18 -0500

    Starting JSONObject template for a character

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
 $(document).ready(function() {
-    console.log("testing testing 123")
+    console.log("testing testing 123");
+
+    //JSONObject to hold all the content
+    var myChar = {
+        'caracteristics' : {
+            'characterName' : "",
+            'homeWord' : "",
+            'divination' : "",
+            'gender' : "",
+            'skinColor' : "",
+            'playerName' : "",
+            'careerPath' : "",
+            'quirk' : "",
+            'build' : "",
+            'hairColor' : "",
+            'height' : "",
+            'weight' : "",
+            'eyeColor' : "",
+            'age' : 0,
+        },
+        'caracteristics' : {
+
+        },
+        'skills' : {
+
+        },
+        'talents' : {
+
+        },
+        'fatePoints' : {
+
+        },
+        'meleeWeapons' : {
+
+        },
+        'missleWeapns' : {
+
+        },
+        'psychicPowers' : {
+
+        },
+        'mutations' : {
+
+        }, 
+        'wounds' :{
+
+        },
+        'armour' : {
+
+        },
+        'gear' : {
+
+        },
+        'insanitly' : {
+            'points': 0
+        },
+        'curruption' : {
+
+        },
+        'wealth' : {
+            
+        }
+    };
 });
